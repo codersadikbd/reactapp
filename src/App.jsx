d08beaa3720c64c984c19998c0da74e956c16d3d
codersadikbd <devsadikbd@gmail.com>
2025-03-24 18:59:40 +0600

    feat: add Header1 and Nav components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header1 from './componenet/Header1';
+import Nav from './componenet/Nav';
 const App = () => {
+
   return (
     <div>
-      
+        <Header1/>
+        <Nav/>  
     </div>
   );
 };
---
+++ src/componenet/Nav.jsx
@@ -0,0 +1,13 @@
+
+
+const nav = () => {
+    return (
+        <div>
+            <input placeholder='my name'/>
+            <button>Home</button>
+            
+        </div>
+    );
+};
+
+export default nav;
