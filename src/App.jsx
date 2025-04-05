5165fb4d34c67461f0573b306879d3c5aa5e59e5
codersadikbd <devsadikbd@gmail.com>
2025-04-05 23:50:52 +0600

    feat: add form elements and integrate Milligram CSS for styling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ import React, { useState } from "react";
 const App = () => {
   return (
     <div className="container">
-      <form></form>
+      <form>
+        <input placeholder="First name" />
+        <br />
+        <input placeholder="Last name" />
+        <br />
+        <select>
+          <option value="">Choose City</option>
+          <option value="">Dhaka</option>
+          <option value="">Chittagong</option>
+        </select>
+        <br />
+        <input type="radio" name="gender" value="male" /> Male
+        <input type="radio" name="gender" value="female" /> Female
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
