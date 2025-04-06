e6482978f343c49ed421cab5cf67c60e0c6474e6
codersadikbd <devsadikbd@gmail.com>
2025-04-07 00:34:18 +0600

    fix: update input handling and restore form state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,46 @@ const App = () => {
     city: "",
     gender: "",
   });
+  const OnInputChange = () => {
+    setFormObj((prevObj) => ({
+      ...prevObj,
+      fName: "",
+      lName: "",
+      city: "",
+      gender: "",
+    }));
+  };
   return (
     <div className="container">
       <form>
-        <input value={FormObj.fName} placeholder="First name" />
+        <input
+          onChange={(e) => {
+            OnInputChange("fName", e.target.value);
+          }}
+          value={FormObj.fName}
+          placeholder="First name"
+        />
         <br />
         <input value={FormObj.lName} placeholder="Last name" />
         <br />
-        <select>
+        <select value={FormObj.city}>
           <option value="">Choose City</option>
           <option value="Dhaka">Dhaka</option>
           <option value="Chittagong">Chittagong</option>
         </select>
         <br />
-        <input type="radio" name="gender" /> Male
-        <input type="radio" name="gender" /> Female
+        <input
+          checked={FormObj.gender === "Male"}
+          type="radio"
+          name="gender"
+        />{" "}
+        Male
+        <input
+          checked={FormObj.gender === "Female"}
+          type="radio"
+          name="gender"
+        />{" "}
+        Female
         <br />
         <button type="submit">Submit</button>
       </form>
