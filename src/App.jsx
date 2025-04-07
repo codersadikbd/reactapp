47360c284c885eb8164165917a6f21e77eea20ea
codersadikbd <devsadikbd@gmail.com>
2025-04-07 15:37:42 +0600

    feat: implement routing and add page components for Home, Product, Profile, and NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React, { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  let [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://dummyjson.com/products/1").then((res) =>
-      res.json().then((json) => setData(json))
-    );
-  }, []);
-  return <div className="container">{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <routes>
+          <route path="/" element={<HomePage />} />
+          <route path="/Product" element={<ProductPage />} />
+          <route path="/Profile" element={<ProfilePage />} />
+          <route path="*" element={<NotFound />} />
+        </routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
